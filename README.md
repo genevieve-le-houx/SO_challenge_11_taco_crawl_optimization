c4200591f1ed4fc389678a9297b89e5eee26a1eb
Geneviève Le Houx <gnvvlhx@gmail.com>
2025-10-19 12:47:45 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Stackoverflow challenge #11: Taco crawl optimization
+
+This is my submission to Stackoverflow challenge #11: Taco crawl optimization
+
+https://stackoverflow.com/beta/challenges/79785895/code-challenge-11-taco-crawl-optimization
+
+The goal of the challenge is to visit 8 out of 10 taco spot, while optimizing to have the best "tastiness score" and travelling the less distance
+
+The tacos data are given in the file tacos_data.csv
+
+# Execution
+The code is done using Python 3.13.7. To execute, simply run main.py. ``dataclasses-json`` library is needed.
---
+++ main.py
@@ -0,0 +1,75 @@
+import csv
+import itertools
+import math
+import time
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import List, Tuple
+
+from dataclasses_json import dataclass_json, config
+
+
+
+@dataclass_json
+@dataclass
+class TacoSpot:
+    number: int = field(metadata=config(field_name="TacoSpot"))
+    x: int
+    y: int
+    tastiness: int = field(metadata=config(field_name="Tastiness"))
+
+
+def get_taco_spots(filepath: Path) -> List[TacoSpot]:
+    data: List[TacoSpot] = []
+
+    with open(filepath, "r") as file:
+        reader = csv.DictReader(file)
+        for line in reader:
+            data.append(TacoSpot.from_dict(line))
+
+    return data
+
+
+def get_distance_between_spots(spot1: TacoSpot, spot2: TacoSpot) -> float:
+    return math.sqrt((spot1.x - spot2.x)**2 + (spot1.y - spot2.y)**2)
+
+
+def get_permutation_score(permutation: Tuple[TacoSpot, ...]) -> Tuple[float, int, float]:
+    total_tastiness = sum(x.tastiness for x in permutation)
+
+    total_distance = 0
+
+    for i, taco_spot in enumerate(permutation[1:], 1):
+        total_distance += get_distance_between_spots(taco_spot, permutation[i - 1])
+
+    return total_tastiness / total_distance, total_tastiness, total_distance
+
+
+def get_best_permutation(permutations: List[Tuple[TacoSpot, ...]]) -> Tuple[Tuple[TacoSpot, ...], float, int, float]:
+    scores, tastiness, distance = zip(*map(get_permutation_score, permutations))
+
+    max_score_index = scores.index(max(scores))
+
+    return (
+        permutations[max_score_index], scores[max_score_index], tastiness[max_score_index],
+        distance[max_score_index]
+    )
+
+
+def main():
+    tic = time.time()
+    data = get_taco_spots(Path("tacos_data.csv"))
+
+    permutations = list(itertools.permutations(data, 8))
+
+    best_comb, max_score, tastiness, distance = get_best_permutation(permutations)
+    print([x.number for x in best_comb])
+    print(f"Tastiness of {tastiness} with a distance of {distance}, so score is {max_score}")
+
+    tac = time.time()
+
+    print(f"Got results in {tac - tic} seconds")
+
+
+if __name__ == '__main__':
+    main()
